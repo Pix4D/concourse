c13fd1ba08dc0b11005c8bb607fba77b693d0225
Alessandro Degano <alessandro.degano@pix4d.com>
2019-10-09 10:53:18 +0200

    Queue up the tasks in limit-active-tasks.
    Provide a DB based queueing system for tasks when
    using the limit-active-tasks strategy.
    
    The ordering is insertion-time based: first arrived
    first served.
    
    Tasks are assigned different queues when specifiying:
    platform, team and tags.
    
    Update package for Squirrel to include bugfix
    for `FromSelect` not working properly.
    
    FYI:
    I was inspired by this talk: https://www.pgcon.org/2016/schedule/attachments/414_queues-pgcon-2016.pdf
    for the design of the Postgres-based queue.
    
    Signed-off-by: Alessandro Degano <alessandro.degano@pix4d.com>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -599,7 +599,8 @@ func (cmd *RunCommand) constructAPIMembers(
 	)
 
 	pool := worker.NewPool(workerProvider)
-	workerClient := worker.NewClient(pool, workerProvider)
+	taskQueue := db.NewTaskQueue(dbConn)
+	workerClient := worker.NewClient(pool, workerProvider, taskQueue)
 
 	credsManagers := cmd.CredentialManagers
 	dbPipelineFactory := db.NewPipelineFactory(dbConn, lockFactory)
@@ -767,7 +768,8 @@ func (cmd *RunCommand) constructBackendMembers(
 	)
 
 	pool := worker.NewPool(workerProvider)
-	workerClient := worker.NewClient(pool, workerProvider)
+	taskQueue := db.NewTaskQueue(dbConn)
+	workerClient := worker.NewClient(pool, workerProvider, taskQueue)
 
 	defaultLimits, err := cmd.parseDefaultLimits()
 	if err != nil {
